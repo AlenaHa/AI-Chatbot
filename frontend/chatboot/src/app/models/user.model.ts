7e4803a3fa12e838e4b534220043d474804772a2
EcaterinaM <ecaterina.manolache96@gmail.com>
2017-12-16 16:36:07 +0200

    added new dependencies and started profile component (#3)

--- frontend/chatboot/src/app/models/user.model.ts
+++ frontend/chatboot/src/app/models/user.model.ts
@@ -1,6 +1,16 @@
 /** exemplu de model */
 
 export class User{
-    username: string;
+    name: string;
+    lastname: string;
+    faculty: string;
+    yearFaculty: number;
     password: string;
+
+    public constructor(_name: string, _lastname: string, _faculty: string, _yearFaculty: number){
+        this.name = _name;
+        this.lastname = _lastname;
+        this.faculty = _faculty;
+        this.yearFaculty = _yearFaculty;
+    }
 }
