acd9fa8834d5e2cfce1f84f4f07aa873dec77765
Madalina <ioanainsuratelu@yahoo.com>
2017-12-22 10:30:19 +0200

    Login + Chatroom (#9)

--- frontend/chatboot/src/app/components/login-component/login.component.ts
+++ frontend/chatboot/src/app/components/login-component/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +9,18 @@ import { Component } from '@angular/core';
 })
 /* fiecare componenta pe care o facem trebuie sa aibe o structura de genu
    si componenta va fi inregistrata in app.module.ts*/
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor(private router: Router) { }
+  
+  redirectOnRegister(){
+    this.router.navigate(['/register']);
+  }
+
+  redirectOnChat() {
+    this.router.navigate(['/chatroom']);
+  }
+
+  ngOnInit() {
+    
+  }
 }
