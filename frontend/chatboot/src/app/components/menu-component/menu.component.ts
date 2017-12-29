ab1557d22b0873f33849cb04012a2fbe68e34004
EcaterinaM <ecaterina.manolache96@gmail.com>
2017-12-29 22:43:17 +0200

    added menu and made small changes in profile component.

--- frontend/chatboot/src/app/components/menu-component/menu.component.ts
+++ frontend/chatboot/src/app/components/menu-component/menu.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
+  constructor(private router:Router){}
 
-  constructor() { }
-
-  ngOnInit() {
+  openNav():void {
+  document.getElementById("mySidenav").style.width = "250px";
   }
 
+closeNav():void {
+  document.getElementById("mySidenav").style.width = "0";
+  }
+
+route(event): void{
+  if(event == 1){
+    this.router.navigate(["/chatroom"]); 
+  }else if(event == 2){
+    this.router.navigate(['/profile']); 
+  }else if( event == 3){
+    this.router.navigate(['/login']); 
+  }
+  this.closeNav();
+  }
 }
