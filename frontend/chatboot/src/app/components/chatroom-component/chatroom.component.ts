6003f988f45294851932f445958e8c76a8825ae7
cameliamacariu <camelia.macariu@yahoo.com>
2018-01-04 16:08:50 +0200

    Create general component for message; (#12)
    User can send message in chatroom

--- frontend/chatboot/src/app/components/chatroom-component/chatroom.component.ts
+++ frontend/chatboot/src/app/components/chatroom-component/chatroom.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
+import { MessageComponent } from '../message-component/message.component';
+import { ChatroomMessageListService } from './chatroom-message-list.service';
 
 @Component({
   selector: 'app-chatroom',
   templateUrl: './chatroom.component.html',
-  styleUrls: ['./chatroom.component.scss']
+  styleUrls: ['./chatroom.component.scss'],
+  providers: [ChatroomMessageListService]
 })
-export class ChatroomComponent implements OnInit {
+export class ChatroomComponent implements OnChanges{
+  private message: string;
+  private textValue: string;
+  
+  constructor(private messageListService: ChatroomMessageListService){}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnChanges(): void {
+    this.messageListService.getMessageList();
   }
 
+  public addMessage(): void {
+    this.message = this.textValue;
+    this.textValue = "";
+    this.messageListService.addMessage(this.message);
+    this.messageListService.getMessageList();    
+  }
 }
