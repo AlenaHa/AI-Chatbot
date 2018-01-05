1debc89e7bd830a492920ae2c705473aedf66a8d
EcaterinaM <ecaterina.manolache96@gmail.com>
2018-01-05 10:06:26 +0200

    Refactoring profile component and added menu component. (#13)
    * Made first improvements to the profile component.
    
    * added menu and made small changes in profile component.
    
    * finalize menu and profile component.

--- frontend/chatboot/src/app/components/profile-component/profile.component.ts
+++ frontend/chatboot/src/app/components/profile-component/profile.component.ts
@@ -9,10 +9,26 @@ import { User } from '../../models/user.model';
 export class ProfileComponent implements OnInit {
 
   private user: User;
-
+  private editIsDisabled: boolean;
+  
   ngOnInit() {
-    this.user = new User('Ecaterina','Manolache','UMF Iasi',3);
-    console.log(this.user);
+    this.user = new User('Prenume','Nume','UMF Iasi','3');
+    this.editIsDisabled = false;
+  }
+
+  private deleteProfileInformation(): void{
+    this.user.lastname = "";
+    this.user.faculty = "";
+    this.user.name = "";
+    this.user.yearFaculty = "";
+  }
+
+  private editProfileInformation(): void{
+    this.editIsDisabled = true;
+  }
+
+  private saveInformation():void{
+    this.editIsDisabled = false;
   }
 
 }
