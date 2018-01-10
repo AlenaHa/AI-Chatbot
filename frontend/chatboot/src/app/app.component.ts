a4a036956c34b7b627984932b251b64878ea9a54
EcaterinaM <ecaterina.manolache96@gmail.com>
2018-01-10 20:26:11 +0200

    resolved menu and chatroom bugs

--- frontend/chatboot/src/app/app.component.ts
+++ frontend/chatboot/src/app/app.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as myGlobals  from 'globals';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isLoggedIn: boolean;
+  showSideBar: boolean;
+  showNav: boolean;
+
+  constructor(
+    private router:Router){
+    this.showNav = true;
+    this.showSideBar = true;
+  }
+    
+  ngOnInit(){
+      this.router.events.subscribe((e) => {
+      if (e instanceof NavigationEnd) {
+        let urlSlice = e.url.toString().substr(0,10);
+        if(urlSlice.indexOf('login')!==-1){
+         this.isLoggedIn= true;
+      
+        }
+        else{
+          this.isLoggedIn= false;      }
+      }
+      });
+  }
 }
