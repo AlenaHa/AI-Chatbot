1b13d22a2d726d955533a9d6cbb4eea68e5074b9
madalina1 <ioanainsuratelu@yahoo.com>
2018-01-12 03:05:20 +0200

    Register page + fixed bugs

--- frontend/chatboot/src/app/components/register-component/register.component.ts
+++ frontend/chatboot/src/app/components/register-component/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+  
+  redirectOnLogin(){
+    this.router.navigate(['/login']);
+  }
 
   ngOnInit() {
   }
