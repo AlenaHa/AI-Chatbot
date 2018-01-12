866afe3a2fe8658fad21e1904a5dd97fb8c2dc05
AlenaHa <andreea.hardon@gmail.com>
2018-01-12 22:25:21 +0200

    New chatbot brain (#19)
    * Modificari importante
    
    * updated some paths in Chatbot.py
    
    * Update Chatbot.py

--- cbot/views/api/chat.py
+++ cbot/views/api/chat.py
@@ -5,13 +5,15 @@ from flask_restful import request, abort
 
 from cbot import schemas, settings, utils
 from cbot.views.api import base
+from cbot.core.Chatbot import Chatbot
 
 
 URL_PREFIX = "/chat"
 
 
 class ChatResource(base.BaseResource):
-
+    def __init__(self):
+        self.bot = Chatbot()
     ENDPOINT = "chat"
 
     def get(self):
@@ -19,6 +21,5 @@ class ChatResource(base.BaseResource):
         msg = request.args.get("message")
         if not msg:
             return {"message": "missing 'message' query parameter"}, 400
-        # TODO(Elena): aici apelam functia care intoarce o replica la mesaj.
-        response = msg[::-1]    # mesajul inversat (de test; a se sterge)
+        response = self.bot.communicate(msg)
         return {"data": response}
